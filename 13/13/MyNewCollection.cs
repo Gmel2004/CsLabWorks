635c7e340795e397aad86a0c7c3a75b135a4ad7f
Gmel2004 <gmelnikob@gmail.com>
2024-04-25 23:26:24 +0500

    upgrade: all

---
+++ 13/13/MyNewCollection.cs
@@ -0,0 +1,93 @@
+﻿using Task;
+using _12;
+
+namespace _13
+{
+    public class MyNewCollection : MySortedSet<Animal>
+    {
+        public enum eventType
+        {
+            AddItem,
+            RemoveItem
+        }
+
+        public delegate void ActionCollection(eventType type, Animal animal);
+        public event ActionCollection? OnItemAdded;
+        public event ActionCollection? OnItemRemoved;
+
+        public string CollectionName { get; set; }
+        private static Journal journal = new Journal();
+
+        public MyNewCollection(string collectionName) => CollectionName = collectionName;
+
+        public new void Add(Animal animal)
+        {
+            base.Add(animal);
+            HandleEvent(eventType.AddItem, animal);
+        }
+
+        public new bool Remove(Animal animal)
+        {
+            bool isRemove = base.Remove(animal);
+            if (isRemove) HandleEvent(eventType.RemoveItem, animal);
+            return isRemove;
+        }
+
+        public void HandleEvent(eventType type, Animal animal)
+        {
+            switch (type)
+            {
+                case eventType.AddItem:
+                    if (OnItemAdded != null) OnItemAdded(type, animal);
+                    break;
+                case eventType.RemoveItem:
+                    if (OnItemRemoved != null) OnItemRemoved(type, animal);
+                    break;
+            }
+        }
+
+        public void RememberChanges(eventType type, Animal animal) =>
+            journal.Add(new JournalEntry(CollectionName, type, animal));
+
+        public static string Changes => journal.ToString();
+
+        private class Journal
+        {
+            public List<JournalEntry> Entries { get; private set; } = new List<JournalEntry>();
+
+            public void Add(JournalEntry entry) => Entries.Add(entry);
+
+            public void Clear() => Entries.Clear();
+
+            public override string ToString()
+            {
+                return $"Entries:\n{string.Join("\n\n", Entries)}";
+            }
+        }
+
+        private class JournalEntry
+        {
+            public string CollectionName { get; private set; }
+            public eventType ActionType { get; private set; }
+            public object ChangedData { get; private set; }
+
+            public JournalEntry(string collectionName, eventType type, object changedData)
+            {
+                CollectionName = collectionName;
+                ActionType = type;
+                ChangedData = changedData;
+            }
+
+            public override string ToString()
+            {
+                return
+                    $"""
+                    Collection: {CollectionName}
+                    Change Type: {ActionType}
+                    Changed Data:
+                    {ChangedData}
+                    """;
+            }
+        }
+    }
+}
--- 13/13/Program.cs
+++ 13/13/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace _13
+﻿using Task;
+
+namespace _13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyNewCollection collection1 = new MyNewCollection("Collection1");
+            MyNewCollection collection2 = new MyNewCollection("Collection2");
+
+            collection1.OnItemAdded += collection1.RememberChanges;
+            collection2.OnItemRemoved += collection2.RememberChanges;
+
+            collection1.Add(new Bird("Kasha", 2, 2, 0.2));
+            collection1.Add(new Animal("Den", 20, 23));
+            collection1.Remove(collection1.First()); // Запись в журнал не добавится
+
+            collection2.Add(new Animal("Jack", 13, 33)); // Запись в журнал не добавится
+            collection2.Add(new Artiodactyl("Ant", 12, 40, Artiodactyl.ArtiodactylSuborder.Tylopoda)); // Запись в журнал не добавится
+            collection2.Remove(collection2.Last());
+
+            Console.WriteLine(MyNewCollection.Changes);
         }
     }
 }
